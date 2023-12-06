e17c9fabbc997bfc27c33e56317d9e6793c4ebfa
Robin Stolpe <robin@stolpe.io>
2023-12-06 21:34:24 +0100

    update

--- DeleteUserProfile/DeleteUserProfile.psm1
+++ DeleteUserProfile/DeleteUserProfile.psm1
@@ -101,34 +101,34 @@ Function Remove-RSUserProfile {
         The name of the remote computer you want to display all of the user profiles from. If you want to use it on a local computer you don't need to fill this one out.
 
         .PARAMETER Delete
-        If you want to delete just one user profile your specify the username here.
+        If you want to delete specific user profiles you can enter the username here.
 
-        .PARAMETER DeleteAll
-        If you want to delete all of the user profiles on the local or remote computer you can set this to $True or $False
-
-        .EXAMPLE
-        Remove-RSUserProfile -DeleteAll
-        # This will delete all of the user profiles from the local computer your running the script from.
+        .PARAMETER All
+        If you want to delete all of the user profiles on the local or remote computer you can use this switch
 
         .EXAMPLE
-        Remove-RSUserProfile -Exclude "User1", "User2" -DeleteAll
+        Remove-RSUserProfile -All
+        # This will delete all of the user profiles from the local computer your running the script from. Beside special and loaded profiles
+
+        .EXAMPLE
+        Remove-RSUserProfile -Exclude "User1", "User2" -All
         # This will delete all of the user profiles except user profile User1 and User2 on the local computer
 
         .EXAMPLE
         Remove-RSUserProfile -Delete "User1", "User2"
-        # This will delete only user profile "User1" and "User2" from the local computer where you run the script from.
+        # This will delete only user profile "User1" and "User2" from the local computer where you run the script from if the profile are not loaded.
 
         .EXAMPLE
-        Remove-RSUserProfile -ComputerName "Win11-test" -DeleteAll
-        # This will delete all of the user profiles on the remote computer named "Win11-Test"
+        Remove-RSUserProfile -Computername "Win11-test" -All
+        # This will delete all of the user profiles that are not special or loaded on the remote computer named "Win11-Test"
 
         .EXAMPLE
-        Remove-RSUserProfile -ComputerName "Win11-test" -Exclude "User1", "User2" -DeleteAll
-        # This will delete all of the user profiles except user profile User1 and User2 on the remote computer named "Win11-Test"
+        Remove-RSUserProfile -Computername "Win11-test" -Exclude "User1", "User2" -All
+        # This will delete all of the user profiles except user profile User1 and User2 on the remote computer named "Win11-Test" if the profile are not loaded
 
         .EXAMPLE
-        Remove-RSUserProfile -ComputerName "Win11-test" -Delete "User1", "User2"
-        # This will delete only user profile "User1" and "User2" from the remote computer named "Win11-Test"
+        Remove-RSUserProfile -Computername "Win11-test" -Delete "User1", "User2"
+        # This will delete only user profile "User1" and "User2" from the remote computer named "Win11-Test" if the profile are not loaded
 
         .LINK
         https://github.com/rstolpe/DeleteUserProfile/blob/main/README.md
@@ -147,7 +147,7 @@ Function Remove-RSUserProfile {
         [Parameter(Mandatory = $false, HelpMessage = "Enter computername on the computer that you to delete user profiles from, multiple names are supported")]
         [string[]]$ComputerName = "localhost",
         [Parameter(Mandatory = $false, HelpMessage = "Enter the name of the user profiles that you want to delete, multiple names are supported")]
-        [string[]]$Delete,
+        [string[]]$Username,
         [Parameter(Mandatory = $false, HelpMessage = "Use this switch if you want to delete all user profiles on the computer")]
         [switch]$All = $false,
         [Parameter(Mandatory = $false, HelpMessage = "Enter username of the user profiles that you want to exclude, multiple names are supported")]
